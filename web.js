2a1f0d6d3d2c202fa6f2d9f2832b59213f810c46
Linda Uruchurtu <yakinodi@gmail.com>
2013-07-20 13:12:51 +0000

    Updated web.js with buffer functions

--- web.js
+++ web.js
@@ -3,7 +3,8 @@ var app = express();
 app.use(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+    response.send(buf.toString(fs.readFile(index.html),'utf8'));
+// response.send('Hello World 2!');
 });
 
 var port = process.env.PORT || 5000;
